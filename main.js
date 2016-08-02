b706fa2905a0752e559e2f43612f3bc727043563
Andrew Tam <andrew.tam.sp@gmail.com>
2016-08-02 17:50:37 -0400

    syntax changes

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ var drone = bebop.createClient();
 console.log("fuck me");
 
 drone.connect(function() {
-    console.log("fuck");
-  drone.takeOff();
+    console.log("fuck it's working");
+    drone.takeOff();
 
-  setTimeout(function() {
-    drone.land();
-  }, 5000);
+    setTimeout(function() {
+        drone.land();
+    }, 5000);
 });
