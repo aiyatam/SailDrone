69eb5c3358e8fb29c24c887423b1c57b027d14eb
Euri Kot <ekot@sailthru.com>
2016-08-03 19:48:51 -0400

    added upload function

--- sailDroneBot.js
+++ sailDroneBot.js
@@ -1,10 +1,17 @@
 var SlackBot = require('slackbots');
+var Slack = require('node-slack-upload')
+
+var fs = require('fs');
+var path = require('path');
+
+
 
 // create a Bot
 var settings = {
     token: process.env.HACK_BOT_API_TOKEN,
     name: 'sail-drone'
 };
+var slack_upload = new Slack(settings.token);
 
 var params = {
     icon_emoji: ':battery:'
@@ -21,9 +28,28 @@ bot.on('start', function() {
     // bot.postMessageToGroup('sail-drone', 'testing...', params);
 });
 
+// var path = "/Users/ekot/dev/saildrone/SailDrone/"
 bot.on('message', function(data) {
     if (data.text && data.text.includes('hello')) {
         bot.postMessageToGroup('sail-drone', 'hey hows it going?', params);
+    }
+    if (data.text && data.text.includes('upload saildrone')) {
+        slack_upload.uploadFile({
+            file: fs.createReadStream(path.join(__dirname, '..', 'SailDrone/drone.jpg')),
+            filename: "drone.jpg",
+            filetype: 'auto',
+            title: 'Sail Drone',
+            initialComment: 'Wow!',
+            channels: 'sail-drone'
+        }, function(err) {
+            if (err) {
+                console.error(err);
+            }
+            else {
+                console.log('done');
+            }
+        });
+
     }
     console.log(data);
 });
