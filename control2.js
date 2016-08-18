aae7e54aeaf7df8e88bfbaed352257b4df9b2914
Johnny Marnell <johnnymarnell@gmail.com>
2016-08-17 21:50:29 -0400

    totes garbage :( :( :( naaaaah fuck dat :(

---
+++ control2.js
@@ -0,0 +1,8 @@
+var drone = require('node-bebop').createClient();
+
+function connect() {
+
+}
+
+//drone.connect(connect);
+connect();
--- server/main.js
+++
@@ -1,7 +0,0 @@
-var drone = require('./util/safe-drone');
-
-drone.connect(onConnect);
-
-function onConnect() {
-    
-}
